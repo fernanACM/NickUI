21db29ceceba8cad9d6cea8f65d19091dd77b712
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-05-06 04:54:17 -0500

    Lib FormsUI

--- src/fernanACM/NickUI/Loader.php
+++ src/fernanACM/NickUI/Loader.php
@@ -13,8 +13,9 @@ use pocketmine\event\Listener;
 
 use pocketmine\utils\Config;
 
-use fernanACM\NickUI\libs\Vecnavium\FormsUI\SimpleForm;
-use fernanACM\NickUI\libs\Vecnavium\FormsUI\CustomForm;
+use Vecnavium\FormsUI\SimpleForm;
+use Vecnavium\FormsUI\CustomForm;
+
 use fernanACM\NickUI\Utils\PluginUtils;
 use fernanACM\NickUI\commands\NickCommand;
 /**use fernanACM\NickUI\commands\HideCommand;*/
