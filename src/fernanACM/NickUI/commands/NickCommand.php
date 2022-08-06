a19e51ea866b211c0517720a9cdbbb9d5ed97fc1
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-08-06 08:51:27 -0500

    Fix permission | added Commando

--- src/fernanACM/NickUI/commands/NickCommand.php
+++ src/fernanACM/NickUI/commands/NickCommand.php
@@ -1,47 +1,52 @@
 <?php
-
+    
+#      _       ____   __  __ 
+#     / \     / ___| |  \/  |
+#    / _ \   | |     | |\/| |
+#   / ___ \  | |___  | |  | |
+#  /_/   \_\  \____| |_|  |_|
+# The creator of this plugin was fernanACM.
+# https://github.com/fernanACM
+    
 namespace fernanACM\NickUI\commands;
 
 use pocketmine\Server;
 use pocketmine\player\Player;
-use pocketmine\plugin\Plugin;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginOwned;
+
+use CortexPE\Commando\BaseCommand;
+
+use fernanACM\NickUI\commands\subcommands\SetSubCommand;
+use fernanACM\NickUI\commands\subcommands\RandomSubCommand;
+use fernanACM\NickUI\commands\subcommands\ResetSubCommand;
+use fernanACM\NickUI\commands\subcommands\HelpSubCommand;
 
 use fernanACM\NickUI\Loader;
 use fernanACM\NickUI\Utils\PluginUtils;
 
-class NickCommand extends Command implements PluginOwned{
+class NickCommand extends BaseCommand{
     
-    private $plugin;
-
-    public function __construct(Loader $plugin){
-        $this->plugin = $plugin;
-        
-        parent::__construct("nick", "§r§fOpen NickUI by §bfernanACM", "§cUse: /nick", ["nickui"]);
-        $this->setPermission("nick.acm");
-        $this->setAliases(["nickui"]);
+    protected function prepare(): void{
+        $this->setPermission("nickui.acm");
+        $this->registerSubCommand(new SetSubCommand("set", "Change your nick with one command."));
+        $this->registerSubCommand(new RandomSubCommand("random", "Change your name to a random nick with one command."));
+        $this->registerSubCommand(new ResetSubCommand("reset", "Return your nick to normal with one command."));
+        $this->registerSubCommand(new HelpSubCommand("help", "Plugin command list."));
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args){
-        if(count($args) == 0){
-            if($sender instanceof Player) {
-                $this->plugin->NickForm($sender);
-                PluginUtils::PlaySound($sender, "random.burp", 1, 3);
-            } else {
-                $sender->sendMessage("Use this command in-game");
-            }
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+        if(!$sender instanceof Player){
+              $sender->sendMessage("Use this command in-game");
+              return;
         }
-        return true;
-    }
-    
-    public function getPlugin(): Plugin{
-        return $this->plugin;
-    }
-
-    public function getOwningPlugin(): Loader{
-        return $this->plugin;
+        if($sender->hasPermission("nickui.acm")){
+         	   Loader::getInstance()->menu->NickMenu($sender);
+               PluginUtils::PlaySound($sender, "random.pop2", 1, 4.5);
+        }else{
+            $sender->sendMessage(Loader::getInstance()->getMessage($sender, "Messages.no-permission"));
+            PluginUtils::PlaySound($sender, "mob.villager.no");
+        }
     }
 }
